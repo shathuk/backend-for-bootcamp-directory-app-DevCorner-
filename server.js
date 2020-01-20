13bb6d640a1b78dcb43bf293bbd84703c6dce309
Kshathu <ksathu.tk@gmail.com>
2020-01-20 22:36:55 +0530

    initial express setup

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ dotEnv.config({path: './config/config.env'});
 const PORT = process.env.PORT || 5000;
 
 App.listen(PORT, 
-    () => {console.log(`Server running in ${process.env.MODE} Mode on port ${PORT}`)}
+    () => {console.log(`now Server running in ${process.env.MODE} Mode on port ${PORT}`)}
 )
